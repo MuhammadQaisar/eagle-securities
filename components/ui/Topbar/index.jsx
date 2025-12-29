2d94242941ab1a726a7a3edcff51fe86afe15eb3
Muhammad Qaisar <muhammadqaisar863@gmail.com>
2025-12-29 10:58:41 -0800

    new UI commit

--- components/ui/Topbar/index.jsx
+++ components/ui/Topbar/index.jsx
@@ -4,7 +4,7 @@ const Topbar = () => {
   return (
     <div className="bg-gradient-to-r from-[#591A5D] to-[#7b2cbf] text-white text-center py-2 px-4 sm:px-6 lg:px-8">
       <div className="overflow-hidden w-full">
-        <p className="text-sm font-medium animate-marquee">
+        <p className="text-sm font-extralight animate-marquee">
           24/7 Live CCTV Monitoring to Prevent Theft, Boost Safety & Improve Efficiency
         </p>
       </div>
